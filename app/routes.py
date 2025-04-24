1c6d9c773890829f12fda2bf33c1693c16a00e32
Mamud Hasan <mail2mamud@gmail.com>
2025-04-24 15:30:53 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.txt')
 git_token = config["dev"]["git_token"]
+
 g = Github(git_token)
 
 app = Flask(__name__)
@@ -74,39 +75,3 @@ def get_org_by_name(orgname):
         return {"status": f"Error accessing GitHub API: {e.status} - {e.data['message']}"}
     except Exception as e:
         return {f"An unexpected error occurred: {e}"}   
-    
-
-# Inprogress
-@app.post("/org/<string:repo_owner>/fork")  
-def create_forked_repo(repo_owner):
-    
-    try:
-        request_data = request.get_json()
-        repo_owner = request_data["repo_owner"]
-        repo_name = request_data["repo_name"]
-        repo = g.get_repo(f"{repo_owner}/{repo_name}")
-        
-        github_user = g.get_user()
-        
-        #myfork = github_user.create_fork(repo)
-
-        
-        #repo = g.get_repo(f"{repo_owner}/{repo_name}")
-        
-        return {
-            "repo_owner": repo_owner, 
-            "repo_name": repo_name}
-        
-
-
-        # Replace with your desired username for the forked repository
-
-        # fork = repo.create_fork(user)
-
-        # Optional: Verify that the fork was created successfully
-      
-    # return 'Store not found', 404
-        
-    except Exception as e:
-        return {f"Exception: {e}"}
-    
